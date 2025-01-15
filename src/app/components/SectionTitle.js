d5be154b448a9bb210266188b1106da50dcb465b
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-01-15 16:56:40 -0300

    added: Componets - ResumeBtn + SectionTitle & Header Styles

---
+++ src/app/components/SectionTitle.js
@@ -0,0 +1,9 @@
+"use client"
+
+export default function ResumeBtn({title}) {
+    return (
+        <h2 className="text-red-900 font-FasterOne">
+            {title}
+        </h2>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import ResumeBtn from "./components/ResumeBtn";
+import SectionTitle from "./components/SectionTitle";
+
 export default function Home() {
   return (
     <>
-      <header className="bg-zinc-900">
-        <a href="#" className="text-red-900 font-Agbalumo">About</a>
-        <a href="#" className="text-red-900 font-Agbalumo">Experience</a>
-        <a href="#" className="text-red-900 font-Agbalumo">Projects</a>
-        <button>Resume</button>
+      <header className="bg-zinc-900 fixed w-full flex justify-end items-center px-4 py-2 gap-3">
+        <a href="#about_section" className="text-red-900 font-Agbalumo text-lg">About</a>
+        <a href="#experience_section" className="text-red-900 font-Agbalumo text-lg">Experience</a>
+        <a href="#projects_section" className="text-red-900 font-Agbalumo text-lg">Projects</a>
+        <ResumeBtn />
       </header>
 
       <div>
@@ -26,19 +29,21 @@ export default function Home() {
 
       </div>
 
-      <div>
+      <div id="about_section">
 
-        <h2 className="text-red-900 font-FasterOne">About Me</h2>
+        <SectionTitle title="About Me" />
+
         <section>
           <p className="font-OpenSans">I'm a web developer looking for my first job oportunity. Both front-end and back-end are fields I'm familiar with. I've been studying web development since 2021 and I've completed some courses during this time. It all started with a Javascript brazilian course from Gustavo Guanabara with his project called Curso em Vídeo, one of the most famous Youtube channels in Brazil. Click here to see his channel. After that, I started both The Odin Project and Harvard's CS50's Web Programming with Python and Javascript. CS50W was my main study and the base of all that I know. There I really learned every technology I'm using today: CSS, Javascript and Django. But, above all that, they really taught me how to code. How to solve a problem searching for a solution by my own, getting erros and trying to fix it. Currently I'm on the "Java and Spring Boot" bootcamp from the DIO (Digital Innovation One). My main technology is React, that I learned on Youtube without courses or bootcamps.</p>
 
-          <button>Resume</button>
-        </section>
+          <ResumeBtn />
+          </section>
 
       </div>
 
-      <div>
-        <h2 className="text-red-900 font-FasterOne">Experience</h2>
+      <div id="experience_section">
+
+        <SectionTitle title="Experience" />
 
         <ul>
           <li>
@@ -69,8 +74,8 @@ export default function Home() {
         </ul>
       </div>
 
-      <div>
-        <h2 className="text-red-900 font-FasterOne">Projects</h2>
+      <div id="projects_section">
+        <SectionTitle title="Projects" />
 
         <ul>
 
@@ -126,7 +131,8 @@ export default function Home() {
       </div>
 
       <div >
-        <h2 className="text-red-900 font-FasterOne">Contacts</h2>
+
+        <SectionTitle title={"Contacts"} />
 
         <ul>
 
