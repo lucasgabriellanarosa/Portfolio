724d24eea32b17a9588fa16b26d1686832096619
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-01-27 13:41:21 -0300

    resume added

--- src/app/components/ResumeBtn.js
+++ src/app/components/ResumeBtn.js
@@ -3,9 +3,9 @@ import { BsDownload } from "react-icons/bs";
 
 export default function ResumeBtn() {
     return (
-        <button className="flex justify-center items-center gap-1 px-2 py-1 rounded-lg text-sm font-OpenSans border-2 font-bold border-red-900 bg-red-900 hover:bg-transparent hover:text-red-900 transition-all duration-300">
+        <a href="./Lucas Gabriel de Lana Rosa - Desenvolvedor Fullstack.pdf" download="Lucas Gabriel de Lana Rosa - Desenvolvedor Fullstack.pdf" className="flex justify-center items-center gap-1 px-2 py-1 rounded-lg text-sm font-OpenSans border-2 font-bold border-red-900 bg-red-900 hover:bg-transparent hover:text-red-900 transition-all duration-300">
             <BsDownload />
             Resume
-        </button>
+        </a>
     )
 }
