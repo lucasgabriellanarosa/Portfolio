e2ce57bb294a6db03aede308b430cfcd84bb124d
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-03-31 15:39:09 -0300

    started inserting basic data with api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import app from "./firebase"
 import { getDatabase, ref, onValue } from "firebase/database";
+import { IoDocumentTextOutline, IoHomeOutline } from 'react-icons/io5';
+import { GoPerson } from 'react-icons/go';
+import { CiFolderOn, CiMail } from 'react-icons/ci';
 
 function App() {
 
@@ -24,17 +27,132 @@ function App() {
       console.error("Error fetching data: ", error);
     });
   };
-  
+
 
 
   useEffect(() => {
     fetchData()
   }, [])
-  
+
 
   console.log(data)
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <header>
+        <div>
+          <img src="#" />
+          <h1>Lucas Gabriel L.R</h1>
+        </div>
+
+        <ul>
+
+          <li>
+            <IoHomeOutline />
+            <a href="#">Home</a>
+          </li>
+
+          <li>
+            <GoPerson />
+            <a href="#">About</a>
+          </li>
+
+          <li>
+            <IoDocumentTextOutline />
+            <a href="#">Experience</a>
+          </li>
+
+          <li>
+            <CiFolderOn />
+            <a href="#">Projects</a>
+          </li>
+
+          <li>
+            <CiMail />
+            <a href="#">Contacts</a>
+          </li>
+
+        </ul>
+
+        <span>Copyright © 2025</span>
+
+      </header>
+      <main>
+
+        <section>
+          <h2>Lucas Gabriel L.R</h2>
+          <h3>I am a web developer</h3>
+        </section>
+
+        <section>
+          <h2>About Me</h2>
+          <p>{data.about_text}</p>
+
+          <button>Resume</button>
+        </section>
+
+        <section>
+          <h2>Experience</h2>
+
+          <div>
+            <h3>Professional</h3>
+
+            <ul>
+              {data.experience > [] ?
+                data.experience.map((experience, key) => (
+                  <li key={key}>
+                    <div>
+                      <img src={experience.img} />
+                      <div>
+                        <p>{experience.title} At {experience.company}</p>
+                        <p>{experience.start_date} - {experience.end_date}</p>
+                        <p>{experience.technologies}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <h4>Responsabilities</h4>
+                      <ul>
+                        {experience.responsabilities.map((responsability, key) => (
+                          <li key={key}>{responsability}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                ))
+
+                :
+                <></>
+              }
+            </ul>
+
+          </div>
+
+          <div>
+            <h3>Education</h3>
+            <div>
+              <ul>
+                {data.education > [] ?
+                  data.education.map((education, key) => (
+                    <li key={key}>
+                      <div>
+                        <img src={education.img} />
+                        <div>
+                          <p>{education.field} At {education.school}</p>
+                          <p>{education.start_date} - {education.end_date}</p>
+                        </div>
+                      </div>
+                    </li>
+                  ))
+
+                  :
+                  <></>
+                }
+              </ul>
+            </div>
+          </div>
+        </section>
+
+      </main>
+    </>
   )
 }
 
